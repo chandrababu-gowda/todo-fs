c379548c39560e533c328f8d4ddf970a4f89e959
Chandrababugowda <chandrababugowda3042003@gmail.com>
2023-09-23 11:39:49 +0530

    Display the task in the client

--- client/index.js
+++ client/index.js
@@ -1,14 +1,32 @@
+var btn = document.querySelector(".submitBtn");
+var taskBox = document.querySelector(".taskMain");
+btn.addEventListener("click", getTask);
+
 function getTask() {
-  fetch("http://localhost:3000")
-    .then((res) => {
-      return res.json();
-    })
-    .then((value) => {
-      console.log(value);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  setTimeout(() => {
+    fetch("http://localhost:3000")
+      .then((res) => {
+        return res.json();
+      })
+      .then((value) => {
+        console.log(value);
+        displayTask(value);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, 100);
+}
+
+function displayTask(taskArr) {
+  let allTask = document.querySelectorAll(".task");
+  allTask.forEach((task) => {
+    task.remove();
+  });
+
+  taskArr.forEach((task) => {
+    taskBox.insertAdjacentHTML("afterend", `<div class="task">${task}</div>`);
+  });
 }
 
 getTask();
